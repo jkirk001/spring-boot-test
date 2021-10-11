23ee8eef2652cdbe104a977926d9c6553ec92826
paylinesales <jone.kirkpatrick@gmail.com>
2021-10-10 21:23:07 -0400

    starting classes and domain(schemas?) for JPA

---
+++ src/main/java/evron/springTest/springWebApp/SpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package evron.springTest.springWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+
+}
